fad671edd9a3058378f96a95d5bcb2cfc058c3a2
alexpp9 <alexandrup.pirtac@outlook.com>
2024-06-11 18:40:08 +0100

    Added Popular Movies to DOM

--- js/script.js
+++ js/script.js
@@ -4,9 +4,38 @@ const global = {
 };
 // Function to display popular movies
 async function displayPopularMovies() {
-  const results = await fetchAPIData('movie/popular');
+  const { results } = await fetchAPIData('movie/popular');
 
-  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    // The image part checks to see whether there's an image for the movie in order to provide a path to it, otherwise, show default
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+            
+            ${
+              movie.poster_path
+                ? `<img
+                  src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                  class="card-img-top"
+                  alt="${movie.title}"
+                />`
+                : `<img
+                  src="images/no-image.jpg"
+                  class="card-img-top"
+                  alt="${movie.title}"
+                />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+    `;
+    document.querySelector('#popular-movies').appendChild(div);
+  });
 }
 
 // Fetch data from TMDB API
