6eb5616f9f42fbd20fbc1c1c17914bd2b9a9f15c
alexpp9 <alexandrup.pirtac@outlook.com>
2024-06-20 16:18:01 +0100

    Added pagination for search results

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: '068f8219a4b8cc950124cd0ff57fab2f',
@@ -258,7 +259,12 @@ async function search() {
   if (global.search.term !== '' && global.search.term !== null) {
     // Take what one needs from results
     // Useful to not repeat the same request from the API, deconstruct
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    // Reset global based on result
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
     // Check to see if there are results
     if (results.length === 0) {
       // Show alert
@@ -277,6 +283,12 @@ async function search() {
 
 // Display search results
 function displaySearchResults(results) {
+  // Clear previous results
+  // Otherwise, when we click next, it'll add new results but it'll keep the old ones as well.
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -314,7 +326,48 @@ function displaySearchResults(results) {
             </p>
           </div>
     `;
+    document.querySelector('#search-results-heading').innerHTML = `
+              <h2>${results.length} of ${global.search.totalResults} results for ${global.search.term}</h2>
+    `;
     document.querySelector('#search-results').appendChild(div);
+  });
+
+  // Display pagination
+  displayPagination();
+}
+
+// Pagination
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+  document.querySelector('#pagination').appendChild(div);
+
+  // Disable <prev> if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  // Disable <next> if on last
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+  // Previous Page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
   });
 }
 
@@ -398,7 +451,7 @@ async function searchAPIData() {
   //   Show spinner before making the request
   showSpinner();
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
   const data = await response.json();
   // Hide spinner
